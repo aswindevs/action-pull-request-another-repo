73a23f9eb7b23f703aa4757b649242ba5dce44ea
Aswin Dev <aswin.d@keyvalue.systems>
2022-07-06 17:44:19 +0530

    refactoring actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,6 +29,8 @@ export GITHUB_TOKEN=$API_TOKEN_GITHUB
 git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
 
+rand=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 4 ;)
+
 echo "Cloning destination git repository"
 git clone "https://$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
@@ -43,7 +45,7 @@ do
 done
 cd "$CLONE_DIR"
 ls -al
-git checkout -b "$INPUT_DESTINATION_HEAD_BRANCH"
+git checkout -b "$INPUT_DESTINATION_HEAD_BRANCH-$rand"
 git status
 git add .
 echo "Adding git commit"
@@ -52,12 +54,12 @@ if (git status | grep -q "Changes to be committed")
 then
   git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
   echo "Pushing git commit"
-  git push -u origin HEAD:$INPUT_DESTINATION_HEAD_BRANCH
+  git push -u origin HEAD:"$INPUT_DESTINATION_HEAD_BRANCH-$rand"
   echo "Creating a pull request"
-  gh pr create -t $INPUT_DESTINATION_HEAD_BRANCH \
-               -b $INPUT_DESTINATION_HEAD_BRANCH \
+  gh pr create -t "$INPUT_DESTINATION_HEAD_BRANCH-$rand" \
+               -b "$INPUT_DESTINATION_HEAD_BRANCH-$rand" \
                -B $INPUT_DESTINATION_BASE_BRANCH \
-               -H $INPUT_DESTINATION_HEAD_BRANCH \
+               -H "$INPUT_DESTINATION_HEAD_BRANCH-$rand" \
                   $PULL_REQUEST_REVIEWERS
 else
   echo "No changes detected"
